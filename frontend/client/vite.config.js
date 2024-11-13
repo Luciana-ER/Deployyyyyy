c9d27aa227ce95715f5e5ec1f140dfba64f46db7
luciana <eromeroluciana@gmail.com>
2024-11-13 08:08:32 -0400

    para hacer deploy en render

--- frontend/client/vite.config.js
+++ frontend/client/vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Esto es importante para que Vite sepa dónde colocar los archivos compilados
+  },
+  base: '/client/', // Especifica la ruta base de tu frontend si está dentro de una carpeta o subdominio
+});
