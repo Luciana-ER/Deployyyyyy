01ab1d2aee03e53a0de77bda15a2166962043335
luciana <eromeroluciana@gmail.com>
2024-11-13 09:06:45 -0400

    borro /api

--- frontend/client/src/api/axios.js
+++ frontend/client/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: 'http://localhost:4000/api',
-  baseURL: `https://deployyyyyy3.onrender.com/api`,
+  baseURL: `https://deployyyyyy3.onrender.com`,
   withCredentials: true,
 });
 
